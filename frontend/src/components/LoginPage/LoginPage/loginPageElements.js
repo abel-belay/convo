90b5824793a579432550382aeb40314a8c6e9a5f
Abel Belay <abel.belay01@gmail.com>
2022-01-30 21:49:14 -0700

    Adjust positoning of login page elements for better mobile UI.

--- frontend/src/components/LoginPage/LoginPage/loginPageElements.js
+++ frontend/src/components/LoginPage/LoginPage/loginPageElements.js
@@ -8,4 +8,9 @@ export const PageWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   background-color: #fbfcfb;
+
+  @media (min-width: 500px) {
+    padding-top: 0;
+    justify-content: center;
+  }
 `;
